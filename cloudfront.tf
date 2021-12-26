b93a49f842dbbf3f41d0c061a01b756ad21aca12
Ankit-Varshney-devops <85929550+Ankit-Varshney-devops@users.noreply.github.com>
2021-12-26 20:27:33 +0530

    Create cloudfront.tf

---
+++ cloudfront.tf
@@ -0,0 +1,76 @@
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.bucket.bucket_regional_domain_name
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = aws_s3_bucket.bucket.bucket_regional_domain_name
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+  }
+
+  price_class = "PriceClass_All"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  custom_error_response {
+    error_code    = 403
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_code    = 404
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "access-identity-"
+}
+
+resource "aws_s3_bucket_policy" "s3policy" {
+  bucket = aws_s3_bucket.bucket.bucket
+  policy = data.aws_iam_policy_document.s3policy.json
+}
+
+output "domain_name" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
+}
